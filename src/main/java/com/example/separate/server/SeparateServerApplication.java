0ccda8bc5c5754074bfea524e22e8329b62fd65e
guohongyang <guohongyang2001@163.com>
2023-07-03 20:21:44 +0800

    创建项目

---
+++ src/main/java/com/example/separate/server/SeparateServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.separate.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeparateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeparateServerApplication.class, args);
+    }
+
+}
